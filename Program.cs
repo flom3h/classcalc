7506fd1fa274affc3648e26148bb3b6f88fbb5a3
flom3h <pieles.florian@gmail.com>
2023-12-12 21:30:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿namespace classcalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rechnung rechnung1 = new Rechnung(100, 20);
+
+            Console.WriteLine($"{rechnung1}");
+
+            while(true)
+            {
+                string opt = PrintMenu1();
+                try
+                {
+                    if (opt == "y")
+                    {
+                        string Prozent = PrintMenu2();
+                        Prozent = Prozent.ToDouble();
+                        rechnung1.PrintRabatt((double)Prozent);
+                    }
+                    else if (opt == "n")
+                    {
+                        rechnung1.PrintBruttopreis();
+                        rechnung1.PrintNettopreis();
+                    }
+                }
+
+                catch (Exception e) 
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+            }
+        }
+
+        static string PrintMenu1()
+        {
+            string menu = @"
+Möchtest du einen Rabatt auf deine Rechnung haben?
+y: 
+n: ";
+            Console.WriteLine(menu);
+
+            string SelectedOption;
+
+            do
+            {
+                Console.Write("Deine Wahl: ");
+                SelectedOption = Console.ReadLine();
+            } while (SelectedOption != "y" && SelectedOption != "n");
+            return SelectedOption;
+        }
+
+        static string PrintMenu2() 
+        {
+            string menu = @"
+Wie viel Rabatt möchtest du haben?
+5:
+10:
+20: ";
+            Console.WriteLine(menu);
+            string chosendiscount;
+            do
+            {
+                Console.Write("Deine Wahl: ");
+                chosendiscount = Console.ReadLine();
+            } while (chosendiscount != "5" && chosendiscount != "10" && chosendiscount != "20");
+            return chosendiscount;
+        }
+    }
+}
---
+++ Rechnung.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classcalc
+{
+    internal class Rechnung
+    {
+        #region FieldVar
+        private double steuer;
+        private double nettopreis;
+        #endregion
+
+        #region Properties
+        public double Steuer 
+        { 
+            get { return steuer; }
+            set { steuer = value; }
+        }
+
+        public double Netto
+        {
+            get { return nettopreis; }
+            set { nettopreis = value; }
+        }
+        #endregion
+
+        #region Const
+        public Rechnung(double steuer, double nettopreis)
+        {
+            Steuer = steuer;
+            Netto = nettopreis;
+        }
+        #endregion
+
+        #region methods
+        public void PrintBruttopreis()
+        {
+            double brutto = Netto / 100 * (100 + Steuer);
+            Console.WriteLine($"Bruttopreis: {brutto.ToString()}");
+        }
+
+        public void PrintNettopreis()
+        {
+            Console.WriteLine($"Nettopreis: {Netto.ToString()}");
+        }
+
+        public void PrintRabatt(double Percent)
+        {
+            double negPercent = (Percent / 100) - 1 ;
+            double posPercent = negPercent * -1;
+            Console.WriteLine($"Alter Preis: {Netto.ToString()}");
+            Console.WriteLine($"Rabatt: {Percent.ToString()}");
+            double brutto = Netto / 100 * (100 + Steuer);
+            Console.WriteLine($"Brutto Preis: {brutto * posPercent}");
+            Console.WriteLine($"Neuer Netto Preis: {Netto * posPercent}");
+        }
+        #endregion
+    }
+}
